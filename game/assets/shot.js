d9ad252d914d59c4e617f7790054d2655a2a7b3f
Willian Oliveira <williangd@yahoo.com.br>
2018-04-07 20:37:14 -0300

    Alteradas cores do drink game

--- game/assets/shot.js
+++ game/assets/shot.js
@@ -1,28 +1,50 @@
-var rollButton = document.getElementById("roll");
-var options = document.getElementsByClassName("option");
+var rollButton = document.getElementById('roll');
+var options = document.getElementsByClassName('option');
 var optionPicked;
 var langButtons = document.querySelectorAll('.lang button');
-var br = ['Beba', 'Dose dupla', 'Beba', 'Jogue novamente', 'Beba', 'Passe a vez', 
-          'Beba', 'Beba e jogue', 'Beba', 'Jogue novamente', 'Beba', 'Passe a vez' ];
-var en = ['Have a shot', 'Double shot', 'Have a shot', 'Play again', 'Have a shot', 'Miss a turn',
-          'Have a shot', 'Drink and play', 'Have a shot', 'Play again', 'Have a shot', 'Miss a turn']
+var br = [
+  'Beba',
+  'Dose dupla',
+  'Beba',
+  'Jogue novamente',
+  'Beba',
+  'Passe a vez',
+  'Beba',
+  'Beba e jogue',
+  'Beba',
+  'Jogue novamente',
+  'Beba',
+  'Passe a vez'
+];
+var en = [
+  'Have a shot',
+  'Double shot',
+  'Have a shot',
+  'Play again',
+  'Have a shot',
+  'Miss a turn',
+  'Have a shot',
+  'Drink and play',
+  'Have a shot',
+  'Play again',
+  'Have a shot',
+  'Miss a turn'
+];
 
-langButtons[0].addEventListener("click", function() {
-  setLanguague('en')
-})
-
-langButtons[1].addEventListener("click", function() {
-  setLanguague('br')
+langButtons[0].addEventListener('click', function() {
+  setLanguague('en');
 });
 
-setLanguague('en')
+langButtons[1].addEventListener('click', function() {
+  setLanguague('br');
+});
 
+setLanguague('en');
 
-rollButton.addEventListener("click", function() {
-  rollButton.setAttribute("disabled", "true");
-  rollButton.style.backgroundColor = "pink";
-  for(var i = 0; i < options.length; i++) {
-    options[i].removeAttribute("id", "selected");
+rollButton.addEventListener('click', function() {
+  rollButton.setAttribute('disabled', 'true');
+  for (var i = 0; i < options.length; i++) {
+    options[i].removeAttribute('id', 'selected');
   }
 
   var times = random(5);
@@ -30,7 +52,7 @@ rollButton.addEventListener("click", function() {
     optionPicked = mark();
     setTimeout(dismark, 500, optionPicked);
     times--;
-    if(times <= 0) {
+    if (times <= 0) {
       endRoll(repeater);
     }
   }, 1000);
@@ -39,9 +61,8 @@ rollButton.addEventListener("click", function() {
 function endRoll(repeater) {
   clearInterval(repeater);
   setTimeout(mark, 1000);
-  setTimeout(function() { 
-    rollButton.removeAttribute("disabled");
-    rollButton.style.backgroundColor = "red"; 
+  setTimeout(function() {
+    rollButton.removeAttribute('disabled');
   }, 1500);
 }
 
@@ -53,25 +74,29 @@ function mark() {
   var num = random(13);
   num = num === 0 ? num : num - 1;
   optionPicked = options[num];
-  optionPicked.setAttribute("id", "selected");
+  optionPicked.setAttribute('id', 'selected');
   return optionPicked;
 }
 
 function dismark(optionPicked) {
-  optionPicked.removeAttribute("id", "selected");
+  optionPicked.removeAttribute('id', 'selected');
 }
 
 function setLanguague(lang) {
-  if(lang === 'br') {
+  if (lang === 'br') {
     setText('Jogar', br);
+    langButtons[0].classList.remove('lang-active');
+    langButtons[1].classList.add('lang-active');
   } else {
     setText('Play', en);
+    langButtons[1].classList.remove('lang-active');
+    langButtons[0].classList.add('lang-active');
   }
 }
 
 function setText(buttonText, lang) {
   rollButton.textContent = buttonText;
-  for(var i = 0; i < options.length; i++) {
+  for (var i = 0; i < options.length; i++) {
     options[i].textContent = lang[i];
   }
-} 
+}
